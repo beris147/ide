36bd338358b24bb8c0fa29e5627c17b50a51f04c
Beristain <edwir147@gmail.com>
2020-02-23 19:45:04 -0600

    añadido el controller

--- src/main/java/com/uaa/idejavafx/PrimaryController.java
+++ src/main/java/com/uaa/idejavafx/PrimaryController.java
@@ -4,5 +4,38 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
+    @FXML
+    private void openNewFile() throws IOException {
+        System.out.println("New file");
+    }
+    
+    @FXML
+    private void saveFile() throws IOException {
+        System.out.println("Save file");
+    }
+    
+    @FXML
+    private void saveFileAs() throws IOException {
+        System.out.println("Save file as...");
+    }
+    
+    @FXML
+    private void openFile() throws IOException {
+        System.out.println("Open file");
+    }
+    
+    @FXML
+    private void undo() throws IOException {
+        System.out.println("undo");
+    }
+    
+    @FXML
+    private void redo() throws IOException {
+        System.out.println("redo");
+    }
+    
+    @FXML
+    private void run() throws IOException {
+        System.out.println("run");
+    }
 }
