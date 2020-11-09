3586514aa3df31eb652803a14ceee3573ce8975f
Daniel Molina <danmnvx@gmail.com>
2020-11-09 01:59:34 -0600

    SymTab structure

--- compiler/semantic/symtab.py
+++ compiler/semantic/symtab.py
@@ -1,3 +1,5 @@
+from semantic.node import SDT
+
 class SymTable:
     def __init__(self) -> None:
         self.vars = {}
@@ -8,9 +10,32 @@ class SymTable:
         else:
             return None
 
-    def insert(self, name, type, lineno):
+    def insert(self, sdt: SDT):
+        name = sdt.data.value
+
         if name in self.vars:
-            self.vars[name]['lines'].append(lineno)
+            self.vars[name]['lines'].append(sdt.data.lineo)
         else:
-            self[name] = {'type': type, 'lines': [lineno], 'val': None}
-        pass
+            self.vars[name] = {'type': sdt.type, 'lines': [sdt.data.lineo], 'val': None}
+    
+    def setAttr(self, name, attribute, value):
+        if name in self.vars:
+            if attribute in self.vars[name]:
+                self.vars[name][attribute] = value
+            else:
+                # throw error
+                pass
+        else:
+            # throw error
+            pass
+
+    def getAttr(self, name, attribute):
+        if name in self.vars:
+            if attribute in self.vars[name]:
+                return self.vars[name][attribute]
+            else:
+                # throw error
+                pass
+        else:
+            # throw error
+            pass
